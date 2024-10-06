c599e3de173cf1fdd166dc6fdb74fa269d3f9eea
Trissolo <00tristano00@gmail.com>
2024-10-06 19:13:29 +0200

    Barely working

--- src/scenes/RayCastDDA.mjs
+++ src/scenes/RayCastDDA.mjs
@@ -90,14 +90,20 @@ export class RayCastDDA extends Phaser.Scene
             -trHalf, trh,
             trHalf, trh,
             0, 0, 0x888899).setOrigin(0);
+
+        this.marker = this.add.rectangle(0, 0, 3, 3, 0xffffff, 1);
     }
 
     gridClicked(pointer, relX, relY)
     {
+        const { line, tr, graphics, edge } = this;
+
+        this.dt.clear()
+        this.updateObstacles();
+
 
         const { x, y } = pointer;
 
-        const { line, tr, graphics, edge } = this;
 
         // Grid Cell
         const actualCellX = Math.floor(relX / edge);
@@ -122,12 +128,139 @@ export class RayCastDDA extends Phaser.Scene
             line.y1 = y;
         }
 
-        const angle = Phaser.Geom.Line.NormalAngle(this.line);
+        const angle = Phaser.Geom.Line.NormalAngle(line);
+
+        const test = new Phaser.Math.Vector2(tr).subtract({x:line.x2, y:line.y2})//.normalize();
+
+
+        // console.log("Angle", angle, new Phaser.Math.Vector2(test).normalize());
+        // console.log("NORMXY", Phaser.Geom.Line.NormalX(line), Phaser.Geom.Line.NormalY(line))
+        const realAngle = Phaser.Geom.Line.Angle(line);
+        const realVec = new Phaser.Math.Vector2().copy(test).setToPolar(realAngle, 1)
+        // console.log("Polar", realVec);
 
         graphics.clear().strokeLineShape(line);
 
         tr.setRotation(angle).setPosition(line.x1, line.y1);
 
+        // new Phaser.Math.Vector2();
+        // lc::vf2d vMouse = { float(GetMouseX()), float(GetMouseY()) };
+		// olc::vf2d vMouseCell = vMouse / vCellSize;
+		// olc::vi2d vCell = vMouseCell; // 
+        
+
+        this.castRay();//new Phaser.Math.Vector2(pointer), new Phaser.Math.Vector2(relX, relY))
+    }
+
+    castRay(vMouse, vMouseCell, vCell = vMouseCell)
+    {
+        const {x1, y1, x2, y2} = this.line;
+        
+        const startCoords = new Phaser.Math.Vector2(x1, y1);
+        const angle = new Phaser.Math.Vector2(this.line.getPointB()).subtract(startCoords).normalize();
+
+        const vRayUnitStepSize = new Phaser.Math.Vector2( Math.abs(1 / angle.x), Math.abs(1 / angle.y));
+
+
+        // Lodev.org also explains this additional optimistaion (but it's beyond scope of video)
+		// olc::vf2d vRayUnitStepSize = { abs(1.0f / vRayDir.x), abs(1.0f / vRayDir.y) };
+
+        const vMapCheck = startCoords.clone();
+
+
+        const vRayLength1D = new Phaser.Math.Vector2();
+
+		const vStep = new Phaser.Math.Vector2();
+
+        if (angle.x < 0)
+            {
+                vStep.x = -1;
+                vRayLength1D.x = (startCoords.x - vMapCheck.x) * vRayUnitStepSize.x;
+            }
+            else
+            {
+                vStep.x = 1;
+                vRayLength1D.x = ((vMapCheck.x + 1) - startCoords.x) * vRayUnitStepSize.x;
+            }
+
+
+            if (angle.y < 0)
+            {
+                vStep.y = -1;
+                vRayLength1D.y = (startCoords.y - vMapCheck.y) * vRayUnitStepSize.y;
+            }
+            else
+            {
+                vStep.y = 1;
+                vRayLength1D.y = (vMapCheck.y + 1 - startCoords.y) * vRayUnitStepSize.y;
+            }
+
+            console.log('%cvRayLength1D', "color: yellow; font-style: italic; background-color: blue;", vRayLength1D);
+
+            console.log("vRayUnitStepSize", vRayUnitStepSize)
+
+            //DDA:
+            let bTileFound = false;
+            let fMaxDistance = 200;
+            let fDistance = 0;
+
+            
+            
+            while (!bTileFound && fDistance < fMaxDistance)
+		    {
+                // Walk along shortest path
+			    if (vRayLength1D.x < vRayLength1D.y)
+                {
+                    vMapCheck.x += vStep.x;
+                    fDistance = vRayLength1D.x;
+                    vRayLength1D.x += vRayUnitStepSize.x;
+                }
+                else
+                {
+                    vMapCheck.y += vStep.y;
+                    fDistance = vRayLength1D.y;
+                    vRayLength1D.y += vRayUnitStepSize.y;
+                }
+    
+                // Test tile at new test point
+                const visSize = this.edge * this.cellsAmount;
+                const vMapSize = {x: visSize, y: visSize};
+                
+                let scallX = Math.floor(vMapCheck.x / this.edge); //vMapSize.x);
+                let scallY = Math.floor(vMapCheck.y / this.edge);//vMapSize.y);
+                
+                const scall = new Phaser.Math.Vector2(scallX, scallY);
+
+                this.dt.fill(0xadadad, 0.4, scallX * this.edge, scallY * this.edge, this.edge, this.edge);//, 10, vIntersection.x, vIntersection.y, 1, 1);
+                console.log("*****vMapCheck", vMapCheck, scall);
+
+                if (vMapCheck.x >= 0 && vMapCheck.x < vMapSize.x && vMapCheck.y >= 0 && vMapCheck.y < vMapSize.y)
+                {
+                    if (this.map[scallY][scallX] !== 0) ///vecMap[vMapCheck.y * vMapSize.x + vMapCheck.x] == 1)
+                    {
+                        bTileFound = true;
+                    }
+                }
+            }
+
+            console.log("Found?", bTileFound)
+
+            const vIntersection = new Phaser.Math.Vector2()
+            if (bTileFound)
+            {
+                vIntersection.setFromObject(startCoords).add(angle.scale(fDistance));//.clone(startCoords).scale(fDistance); // + vRayDir * fDistance;
+                console.log("Inters draw", vIntersection, startCoords, fDistance);
+            }
+
+            if (bTileFound)
+            {
+                //DrawCircle(vIntersection * vCellSize, 4.0f, olc::YELLOW);
+                // this.dt.clear()
+                
+                this.dt.fill(0xffff00, 1, vIntersection.x, vIntersection.y, 1, 1);
+                this.marker.setPosition( vIntersection.x, vIntersection.y);
+                console.log('INTERS', vIntersection)
+            }
     }
 
     buildRandomMap(cellsAmount = 8, toll = 0.6)
@@ -173,13 +306,13 @@ export class RayCastDDA extends Phaser.Scene
 
                 if (curr)
                 {
-                    dt.fill(0x656565, 1, x * edge, y * edge, edge, edge);
+                    dt.fill(0x56aa56, 1, x * edge, y * edge, edge, edge);
                 }
             }
 
             res += "\n";
         }
 
-        console.log(res, map)
+        // console.log(res, map)
     }
 }  // end Scene Class
